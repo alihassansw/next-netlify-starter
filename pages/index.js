fa5fe047529e221ad2675a6545d23ddb9f462f84
Ali Hassan <101920031+alihassansw@users.noreply.github.com>
2024-05-08 23:56:03 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,6 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <iframe name="preview" sandbox="allow-same-origin allow-scripts allow-modals" src="https://d3nv1vpfin7fut.cloudfront.net/" style="width:100%;border: none;height: 100rem;" loading="lazy"></iframe>
   )
 }
